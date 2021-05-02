2ebb9f6c73de0fc5e4ce77906219369b0bbb4349
Eliseu Videira <eliseuvideira1@gmail.com>
2021-05-01 23:04:10 -0300

    :bug: fixing bug not validating input, removing routes

--- src/routes/mails.ts
+++ src/routes/mails.ts
@@ -1,19 +1,21 @@
+import { body } from "@ev-fns/validation";
 import { Router } from "express";
 import { mailsPost } from "../endpoints/mails";
 import { auth } from "../middlewares/auth";
+import { mailsPostBody } from "../validations/mails";
 
 const router = Router();
 
 /**
  * POST /mails
  * @tag mails
- * @security BearerAuth
+ * @security bearerAuth
  * @bodyContent {mailsPostRequestBody} application/json
  * @response 200
  * @responseContent {mailsPostResponseBody} 201.application/json
  * @response default
- * @responseContent {Error} default.application/json
+ * @responseContent {error} default.application/json
  */
-router.post("/mails", auth, mailsPost);
+router.post("/mails", auth, body(mailsPostBody), mailsPost);
 
 export default router;
